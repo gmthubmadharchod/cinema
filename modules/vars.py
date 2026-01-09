24b5369839b59e1ca41a6efd43ffa66b9c88a91f
Ankitrana7777 <rajkumar567jio@gmail.com>
2026-01-10 00:23:06 +0530

    Update API credentials and user details

--- modules/vars.py
+++ modules/vars.py
@@ -3,18 +3,18 @@
 import os
 from os import environ
 
-API_ID = int(environ.get("API_ID", "37138438"))
-API_HASH = environ.get("API_HASH", "d1b5f3a54b0c8130747432aaca542b77")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
+API_ID = int(environ.get("API_ID", "22182189"))
+API_HASH = environ.get("API_HASH", "5e7c4088f8e23d0ab61e29ae11960bf5")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8048022999:AAF4hvMZO868n31Mlqf1A3whZ0c5Vi7-tko")
 
-OWNER = int(environ.get("OWNER", "8306791018"))
-CREDIT = environ.get("CREDIT", '𝐒нɑᎥ𝚝ɑη')
+OWNER = int(environ.get("OWNER", "5302639580"))
+CREDIT = environ.get("CREDIT", 'Ankit')
 cookies_file_path = os.getenv("cookies_file_path", "youtube_cookies.txt")
 
-TOTAL_USER = os.environ.get('TOTAL_USERS', '8306791018,8219124879').split(',')
+TOTAL_USER = os.environ.get('TOTAL_USERS', '5302639580').split(',')
 TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
 
-AUTH_USER = os.environ.get('AUTH_USERS', '5850397219,8219124879').split(',')
+AUTH_USER = os.environ.get('AUTH_USERS', '5302639580').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
@@ -33,6 +33,7 @@ photoyt = 'https://tinypic.host/images/2025/03/18/YouTube-Logo.wine.png' #https:
 photocp = 'https://tinypic.host/images/2025/03/28/IMG_20250328_133126.jpg'
 photozip = 'https://envs.sh/cD_.jpg'
 # .....,.....,.......,...,.......,....., .....,.....,.......,...,.
+
 
 
 
